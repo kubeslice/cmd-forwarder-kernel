07015fec4fad97f594af3b9899930d2a36985473
Bharath Horatti <bharath@aveshasystems.com>
2022-12-14 09:07:30 +0000

    Fixed a couple of logs
    Signed-off-by: Bharath Horatti <bharath@aveshasystems.com>

--- internal/networkservice/xconnect/client.go
+++ internal/networkservice/xconnect/client.go
@@ -3,12 +3,9 @@ package xconnect
 import (
 	"context"
 
-	//      "github.com/pkg/errors"
-
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/networkservicemesh/api/pkg/api/networkservice"
 	"github.com/networkservicemesh/sdk/pkg/networkservice/core/next"
-	"github.com/networkservicemesh/sdk/pkg/tools/log"
 
 	"github.com/kubeslice/cmd-forwarder-kernel/internal/tools/mechanismmetadata"
 
@@ -23,33 +20,17 @@ func NewClient() networkservice.NetworkServiceClient {
 }
 
 func (x *xconnectClient) Request(ctx context.Context, request *networkservice.NetworkServiceRequest, opts ...grpc.CallOption) (*networkservice.Connection, error) {
-	logger := log.FromContext(ctx).WithField("xconnectClient", "Request")
-	logger.Infof("BBH: Request: %v", request)
 	conn, err := next.Client(ctx).Request(ctx, request, opts...)
 	if err != nil {
 		return nil, err
 	}
 
-	logger.Infof("BBH2: conn: %v", conn)
-
 	mechanismmetadata.Store(ctx, true, conn.GetMechanism())
-
-	/*
-
-	   if err := create(ctx, conn, metadata.IsClient(v)); err != nil {
-	           if _, closeErr := v.Close(ctx, conn, opts...); closeErr != nil {
-	                   err = errors.Wrapf(err, "connection closed with error: %s", closeErr.Error())
-	           }
-
-	           return nil, err
-	   }
-	*/
 
 	return conn, nil
 }
 
 func (x *xconnectClient) Close(ctx context.Context, conn *networkservice.Connection, opts ...grpc.CallOption) (*empty.Empty, error) {
-	//logger := log.FromContext(ctx).WithField("vethClient", "Close")
 	_, err := next.Client(ctx).Close(ctx, conn, opts...)
 
 	mechanismmetadata.Delete(ctx, true)
