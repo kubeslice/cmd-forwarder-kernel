77e8acdd5824d5d0fb978bcb1fd75aa8d3a4eaf1
Bharath Horatti <bharath@aveshasystems.com>
2023-06-14 06:35:45 +0000

    fix(): Enable NSC connection over tcp socket
    Added license headers
    
    Signed-off-by: Bharath Horatti <bharath@aveshasystems.com>

--- internal/networkservice/mechanisms/sendfd/server.go
+++ internal/networkservice/mechanisms/sendfd/server.go
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (c) 2023 Avesha, Inc. All rights reserved.
+ *
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package sendfd
 
 import (
@@ -23,9 +41,9 @@ func (s sendFDServer) Request(ctx context.Context, request *networkservice.Netwo
 		return nil, err
 	}
 
-	// Send the FD and swap the FileURL for an InodeURL
+	// Swap the FileURL for an InodeURL
 	inodeURLToFileURLMap := make(map[string]string)
-	if err := sendFDAndSwapFileToInode(conn.GetMechanism().GetParameters(), inodeURLToFileURLMap); err != nil {
+	if err := swapFileToInode(conn.GetMechanism().GetParameters(), inodeURLToFileURLMap); err != nil {
 		return nil, err
 	}
 	return conn, nil
@@ -40,7 +58,7 @@ func (s sendFDServer) Close(ctx context.Context, conn *networkservice.Connection
 
 	// Send the FD and swap the FileURL for an InodeURL
 	inodeURLToFileURLMap := make(map[string]string)
-	if err := sendFDAndSwapFileToInode(conn.GetMechanism().GetParameters(), inodeURLToFileURLMap); err != nil {
+	if err := swapFileToInode(conn.GetMechanism().GetParameters(), inodeURLToFileURLMap); err != nil {
 		return nil, err
 	}
 
